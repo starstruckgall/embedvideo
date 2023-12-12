355e2513a7b86931bf5c728f99af62004945e96a
starstruckgall <146040178+starstruckgall@users.noreply.github.com>
2023-12-13 01:26:56 +0200

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,20 @@
+
+import webbrowser
+video = input("Please paste the link of the youtube video you wish to embed: ")
+
+#get the video id
+video_id = video[32:]
+
+link = 'https://www.youtube.com/embed/' + video_id
+text = '<iframe width="720" height="405" src='+link+' title="Go Programming Tutorial – 3 Beginner Projects"\
+frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+
+read = open('readme.html', 'w')
+read.write(text)
+read = open("readme.html", "r")
+read.read()
+
+
+webbrowser.open('readme.html') 
+
+
